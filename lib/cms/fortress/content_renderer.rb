b9052eaa49de059cf12eb278aa5a08d8c5634bcc
Melvin Sembrano <melvinsembrano@gmail.com>
2014-01-07 12:17:02 +1100

    fixed caching setupj

--- lib/cms/fortress/content_renderer.rb
+++ lib/cms/fortress/content_renderer.rb
@@ -7,13 +7,17 @@ module Cms
         base.class_eval do
 
           def render_html(status = 200)
-            fresh_when etag: @cms_page, last_modified: @cms_page.updated_at.utc, public: true
-            response.cache_control[:max_age] = @cms_page.cached_timeout.seconds
+            cached_timeout = @cms_page.cached_timeout.to_i
+
+            if cached_timeout > 0
+              fresh_when etag: @cms_page, last_modified: @cms_page.updated_at.utc, public: true
+              response.cache_control[:max_age] = cached_timeout.seconds
+            end
 
             if @cms_layout = @cms_page.layout
 
               app_layout = (@cms_layout.app_layout.blank? || request.xhr?) ? false : @cms_layout.app_layout
-              render(:inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => 'text/html') unless performed?
+              render(:inline => @cms_page.content, :layout => app_layout, :status => status, :content_type => 'text/html') unless cached_timeout > 0 && performed?
             else
               render :text => I18n.t('cms.content.layout_not_found'), :status => 404
             end
