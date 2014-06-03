8a5a1804615bc119184ddb15403c932fa4bfbb05
Andy Wenk <andy@nms.de>
2014-06-03 19:26:03 +0200

    remove unused test

--- test/test_cms-fortress.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TestCmsFortress < MiniTest::Test
-  # should "probably rename this file and start testing for real" do
-  #   flunk "hey buddy, you should probably rename this file and start testing for real"
-  # end
-end
