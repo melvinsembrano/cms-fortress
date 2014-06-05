7a82b65bbfa4492335881d633d6ca77eaf13372b
Andy Wenk <andy@nms.de>
2014-06-05 18:27:38 +0200

    fox test for settings

--- test/unit/cms/fortress/settings_test.rb
+++ test/unit/cms/fortress/settings_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class Cms::Fortress::SettingsTest < ActiveSupport::TestCase
 
   test "it_should_raise_MissingConfigFile_exception" do
-    assert_raises (Cms::Fortress::Error::MissingConfigFile) {
+    assert_raises (Cms::Fortress::Error::MissingSettingsFile) {
       Cms::Fortress::Settings.new(:bla)
     }
   end
