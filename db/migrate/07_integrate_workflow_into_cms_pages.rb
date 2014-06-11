d23fd183f8070156721e601a190e0d05d7391d78
Melvin Sembrano <melvinsembrano@gmail.com>
2014-06-11 13:04:54 +1000

    fixed migration of page workflow data

--- db/migrate/07_integrate_workflow_into_cms_pages.rb
+++ db/migrate/07_integrate_workflow_into_cms_pages.rb
@@ -9,23 +9,29 @@ class IntegrateWorkflowIntoCmsPages < ActiveRecord::Migration
     add_column :comfy_cms_pages, :aasm_state, :string, default: 'new'
     add_column :comfy_cms_pages, :published_date, :date
 
+    Comfy::Cms::Page.reset_column_information
+
     Cms::PageWorkflow.all.each do |workflow|
-      workflow.page.aasm_state = case workflow.status_id.to_i
-        when 0
-          'drafted'
-        when 1
-          'reviewed'
-        when 2
-          'scheduled'
-        when 3
-          'published'
-        else
-          'drafted'
-                                 end
-      workflow.page.published_date = workflow.published_date
-      workflow.save!
+      page = Comfy::Cms::Page.where(id: workflow.cms_page_id).first
+      if page
+        page.aasm_state = case workflow.status_id.to_i
+          when 0
+            'drafted'
+          when 1
+            'reviewed'
+          when 2
+            'scheduled'
+          when 3
+            'published'
+          else
+            'drafted'
+          end
+        page.published_date = workflow.published_date
+        raise page.errors.inspect unless page.save!
+      end
     end
 
-    drop_table :cms_page_workflows
+    # TODO: Just to make it safe workflow table will be removed on the 5th patch release
+    # drop_table :cms_page_workflows
   end
 end
