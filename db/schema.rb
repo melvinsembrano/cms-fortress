19fd0d0fdef7dad0c283c09f51cf8f6b9aa28ec4
Matthias Bauer <moeffju@moeffju.net>
2015-03-17 16:08:24 +0100

    Update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 7) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "cms_fortress_role_details", force: true do |t|
     t.string   "name"
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 7) do
     t.integer  "site_id"
   end
 
-  add_index "cms_fortress_users", ["email"], name: "index_cms_fortress_users_on_email", unique: true
+  add_index "cms_fortress_users", ["email", "site_id"], name: "index_cms_fortress_users_on_email_and_site_id", unique: true
   add_index "cms_fortress_users", ["reset_password_token"], name: "index_cms_fortress_users_on_reset_password_token", unique: true
 
   create_table "comfy_cms_blocks", force: true do |t|
