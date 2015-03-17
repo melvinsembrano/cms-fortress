286700cfc68ce556127dbef975631f6dcd689881
Matthias Bauer <moeffju@moeffju.net>
2015-03-17 16:02:28 +0100

    Users per Site

--- app/models/cms/fortress/user.rb
+++ app/models/cms/fortress/user.rb
@@ -5,8 +5,16 @@ class Cms::Fortress::User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+  # manually add :validatable validations because uniqueness does not include :site_id
+  validates_presence_of   :email, if: :email_required?
+  validates_uniqueness_of :email, scope: :site_id, allow_blank: true, if: :email_changed?
+  validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+  validates_presence_of     :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+  validates_length_of       :password, within: Devise.password_length, allow_blank: true
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
+         :recoverable, :rememberable, :trackable, :timeoutable,
+         :request_keys => [:site_id]
 
   belongs_to :role
   belongs_to :site, class_name: "Comfy::Cms::Site", foreign_key: :site_id
@@ -20,6 +28,10 @@ class Cms::Fortress::User < ActiveRecord::Base
     }
   end
 
+  def self.find_for_authentication(warden_conditions)
+    where(:email => warden_conditions[:email], :site_id => warden_conditions[:site_id]).first
+  end
+
   def type
     self.class.types[type_id]
   end
@@ -27,4 +39,15 @@ class Cms::Fortress::User < ActiveRecord::Base
   def display_name
     "#{ email } (#{ type.to_s.titleize })"
   end
+
+protected
+
+  def password_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  def email_required?
+    true
+  end
+
 end
